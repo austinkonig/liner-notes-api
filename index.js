1e36f9a4bda033d624db71b8bf2a332fba3d94df
AustinKDrummer <austinkemail@gmail.com>
2023-08-30 20:09:16 -0700

    new

--- index.js
+++ index.js
@@ -27,7 +27,3 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
