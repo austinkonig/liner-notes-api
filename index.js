51f55a518cd3b19a262ae0a4e9e6ffd54216ea92
AustinKDrummer <austinkemail@gmail.com>
2023-08-30 20:22:30 -0700

    using cors for origin, methods, etc in index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://liner-notes.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors({
+  origin: 'https://liner-notes.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
